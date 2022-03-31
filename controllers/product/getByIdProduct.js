2b338c882f976f3878371a2e59da9a24d8640751
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-03-31 14:55:33 -0300

    feat(getByIdProduct): implementing control that returns user by id

--- controllers/product/getByIdProduct.js
+++ controllers/product/getByIdProduct.js
@@ -1,12 +1,13 @@
 const productService = require('../../services/productService');
 
-const getAll = async (req, res, next) => {
+const getById = async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const products = await productService.getAll();
+    const products = await productService.getById(id);
     return res.status(products.code).json(products.payload);
   } catch (e) {
     next(e);
   }
 };
 
-module.exports = getAll;
+module.exports = getById;
---
+++ controllers/product/index.js
@@ -0,0 +1,7 @@
+const getAllProduct = require('./getAllProduct');
+const getByIdProduct = require('./getByIdProduct');
+
+module.exports = {
+  getAllProduct,
+  getByIdProduct,
+};
