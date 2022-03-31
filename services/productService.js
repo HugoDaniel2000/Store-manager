d78653fa0a5e845fdec2c13bd97d7a339da3d6da
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-03-31 18:05:24 -0300

    feat(productService): validate model return and return to controller

--- services/productService.js
+++ services/productService.js
@@ -9,7 +9,6 @@ const getAll = async () => {
 };
 
 const getById = async (id) => {
-  console.log(id);
   const product = await productsModel.getById(id);
   if (!product.length) {
     return { code: 404, payload: { message: 'Product not found' } };
@@ -17,4 +16,14 @@ const getById = async (id) => {
   return { code: 200, payload: product[0] };
 };
 
-module.exports = { getAll, getById };
+const register = async (name, quantity) => {
+  const products = await productsModel.getAll();
+  const verify = products.some((elem) => elem.name === name);
+  if (verify) {
+    return { code: 409, payload: { message: 'Product already exists' } };
+  }
+  const product = await productsModel.register(name, quantity);
+  return { code: 201, payload: product };
+};
+
+module.exports = { getAll, getById, register };
