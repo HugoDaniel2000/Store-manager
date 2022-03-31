cc513c760f9a86dcbebd73668f54fc317580cbbf
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-03-31 16:29:07 -0300

    feat(saleModel): function search sale by id

--- models/saleModel.js
+++ models/saleModel.js
@@ -2,10 +2,11 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const query = (
-    `SELECT salesProduct.sale_id AS saleId,
-     sales.date,
-     salesProduct.product_id AS productId,
-     salesProduct.quantity
+    `SELECT
+        salesProduct.sale_id AS saleId,
+        sales.date,
+        salesProduct.product_id AS productId,
+        salesProduct.quantity
      FROM StoreManager.sales_products AS salesProduct
      INNER JOIN StoreManager.sales AS sales
      ON salesProduct.sale_id =  sales.id
@@ -15,4 +16,20 @@ const getAll = async () => {
   return result;
 };
 
-module.exports = { getAll };
+const getById = async (id) => {
+  const query = (
+    `SELECT
+        sales.date,
+        salesProduct.product_id AS productId,
+        salesProduct.quantity
+     FROM StoreManager.sales_products AS salesProduct
+     INNER JOIN StoreManager.sales AS sales
+     ON salesProduct.sale_id =  sales.id
+     WHERE salesProduct.sale_id = ?
+     ORDER BY salesProduct.product_id ASC, salesProduct.sale_id ASC
+     `);
+  const [result] = await connection.execute(query, [id]);
+  return result;
+ };
+
+ module.exports = { getAll, getById };
