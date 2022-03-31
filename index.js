fdcba74ddfa56113bd75e20e8c694e2c82bd33d6
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-03-31 14:25:08 -0300

    feat: implementing routes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
+const express = require('express');
+const productRouter = require('./routers/productRoute');
+const error = require('./middlewares/error');
 
 // não remova esse endpoint, e para o avaliador funcionar
+const app = express();
+app.use(express.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productRouter);
+
+app.use(error);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
