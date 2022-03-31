28cc02f05e5b06e30e95c41d6cd616dc6ada0e47
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-03-31 16:33:15 -0300

    feat(saleRoute): creating route to search by id

--- routers/saleRoute.js
+++ routers/saleRoute.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const sales = require('../controllers/sales/index');
 
+router.get('/:id', sales.getByIdSale);
 router.get('/', sales.getAllSales);
 
 module.exports = router;
