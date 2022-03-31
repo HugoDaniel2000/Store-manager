bdf6d93e095f76b6ad6e6068e95ffdbe4db856b8
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-03-31 18:07:45 -0300

    feat(productRouter): creating post method route to /products

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const products = require('../controllers/product/index');
+const middleware = require('../middlewares/productMiddleware');
 
 router.get('/:id', products.getByIdProduct);
+
 router.get('/', products.getAllProduct);
+router.post('/',
+  middleware.validateName,
+  middleware.validateQuantity,
+  products.registerPoduct);
 
 module.exports = router;
