74060521bd66e6538b1d3e34175b4562d639480d
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-03-31 14:50:43 -0300

    feat(productModel): function that takes the product by id

--- models/productModel.js
+++ models/productModel.js
@@ -1,8 +1,15 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [result] = await connection.execute('SELECT * FROM StoreManager.products');
+  const query = 'SELECT * FROM StoreManager.products';
+  const [result] = await connection.execute(query);
   return result;
 };
 
-module.exports = { getAll };
+const getById = async (id) => {
+ const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+ const [result] = await connection.execute(query, [id]);
+ return result;
+};
+
+module.exports = { getAll, getById };
