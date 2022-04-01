b6cd787c4b2cddb97506f27a6c70782ccc6f35c0
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-04-01 00:33:36 -0300

    feat(saleRoute): implement route post endpoint /sales

--- routers/saleRoute.js
+++ routers/saleRoute.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const sales = require('../controllers/sales/index');
+const middleware = require('../middlewares/saleMiddleware');
 
 router.get('/:id', sales.getByIdSale);
 router.get('/', sales.getAllSales);
+
+router.post('/',
+    middleware.validateProductId,
+    middleware.validateQuantity,
+    sales.createSales);
 
 module.exports = router;
