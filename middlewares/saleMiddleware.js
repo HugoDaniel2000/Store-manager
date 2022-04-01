9c6a918b1d926d4378c6284f1b3e6c8bd2dc6092
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-04-01 00:32:23 -0300

    feat(saleMiddelware): validate req body

--- middlewares/saleMiddleware.js
+++ middlewares/saleMiddleware.js
@@ -2,19 +2,23 @@ const BAD_REQUEST = 400;
 const UNPROCESSABLE_ENTITY = 422;
 
 const validateProductId = (req, res, next) => {
-  const { productId } = req.body;
-  if (!productId) {
+  const array = req.body;
+  const isNumber = array.some((e) => Number.isInteger(e.productId));
+  if (!isNumber) {
     return res.status(BAD_REQUEST).json({ message: '"productId" is required' });
   }
   next();
 };
 
 const validateQuantity = (req, res, next) => {
-  const { quantity } = req.body;
-  if (!quantity) {
+  const array = req.body;
+
+  const isUndefined = array.some((e) => e.quantity === undefined);
+  if (isUndefined) {
   return res.status(BAD_REQUEST).json({ message: '"quantity" is required' });
   }
-  if (quantity <= 0) {
+  const isGreaterEqualZero = array.some((e) => e.quantity <= 0);
+  if (isGreaterEqualZero) {
   return res.status(UNPROCESSABLE_ENTITY)
     .json({ message: '"quantity" must be greater than or equal to 1' });
   }
